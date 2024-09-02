747475fd51b29cde7f45356022f95f6175fb68e0
rawanyoussef <rawanyoussef@LAPTOP-3V5FQ72L>
2024-09-02 03:00:24 +0300

    Add project files.

---
+++ Controllers/DepartmentController1.cs
@@ -0,0 +1,22 @@
+﻿using Company.Reposatory.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Company.web.Controllers
+{
+    public class DepartmentController1 : Controller
+    {
+        private readonly IDepartmentReposatory _departmentReposatory;
+
+        public DepartmentController1(IDepartmentReposatory departmentReposatory)
+        {
+            _departmentReposatory = departmentReposatory;
+        }
+        public IActionResult Index()
+        {
+          var departments =_departmentReposatory.GetAll();
+
+            return View(departments);
+        }
+    }
+}
